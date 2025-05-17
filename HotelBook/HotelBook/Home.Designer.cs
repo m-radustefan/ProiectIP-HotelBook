db9dc6b53a2a1d04cd892aa6ba3edd55cc8f21bc
Padurariu20003 <matei-ionut.padurariu@student.tuiasi.ro>
2025-05-17 16:07:12 +0300

    Test2

--- HotelBook/HotelBook/Home.Designer.cs
+++ HotelBook/HotelBook/Home.Designer.cs
@@ -32,6 +32,7 @@
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -67,11 +68,21 @@
             this.richTextBox3.TabIndex = 3;
             this.richTextBox3.Text = "";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(192, 159);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox2);
@@ -88,6 +99,7 @@
         private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
